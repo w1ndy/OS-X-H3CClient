f6b47b81bd6b527c4ae31bbdf476bef7c81b2695
5Ki3s0x1C9 <mystery.wd@gmail.com>
2014-06-08 03:23:02 +0800

    No restart required after installing.

--- Installer/postinstall.sh
+++ Installer/postinstall.sh
@@ -2,6 +2,7 @@
 
 cp /Applications/OS-X-H3CClient.app/Contents/Resources/org.wireshark.ChmodBPF.plist /Library/LaunchDaemons
 chown root:wheel /Library/LaunchDaemons/org.wireshark.ChmodBPF.plist
+chgrp admin /dev/bpf*
+chmod g+rw /dev/bpf*
 
 exit 0
-
